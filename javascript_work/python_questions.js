464bab4192a8461c200b92486802b240828be10e
Syed Omar <syed.omar@ucalgary.ca>
2026-02-06 23:45:17 -0700

    adding a new python question and a file for unix question bank

--- javascript_work/python_questions.js
+++ javascript_work/python_questions.js
@@ -69,7 +69,7 @@ let python_question_6 = {
     difficulty: 'Easy',
     question: 'What is printed from the following code: <br> name = "John" <br> print(type(name))',
     options: [
-        "&lt;class 'int'&gt;", // Use HTML entities for < and >
+        "&lt;class 'int'&gt;",
         "&lt;class 'str'&gt;",
         "&lt;class 'bool'&gt;",
         'Returns an error',
@@ -83,10 +83,30 @@ let python_question_6 = {
     },
 };
 
+let python_question_7 = {
+    language: 'Python',
+    type: 'Multiple Choice',
+    difficulty: 'Easy',
+    question: 'What happins with the code below in Python: <br> d = 7 <br> D = "Silly',
+    options: [
+        "A will overwrite a",
+        "a will overwrite A",
+        "Neither variable will be overwritten",
+        "Error shows up",
+    ],
+    solution: "Neither variable will be overwritten", // This MUST match the option exactly
+    randomize: function() {
+        for (let i = this.options.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.options[i], this.options[j]] = [this.options[j], this.options[i]];
+        }
+    },
+};
+
 
 
 export function python_question_generator(){
     console.log(`Programming Language: Python`);
-    return [python_question_1, python_question_2, python_question_3, python_question_4, python_question_5, python_question_6];
+    return [python_question_1, python_question_2, python_question_3, python_question_4, python_question_5, python_question_6, python_question_7];
 };
 
